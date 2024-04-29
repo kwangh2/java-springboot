33cabe5cb05efdd4020504d4b894e64bcc87a501
kwangh2 <144658667+kwangh2@users.noreply.github.com>
2024-04-29 21:59:25 +0800

    test

---
+++ src/main/java/io/cloudtype/Demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package java.io.cloudtype.Demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "hello-test";
+    }
+}
