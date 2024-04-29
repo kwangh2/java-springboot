da1bd28d6c7051a9bcffe723b2b1a1339c8d9a55
kwangh2 <144658667+kwangh2@users.noreply.github.com>
2024-04-29 20:04:16 +0800

    Update TestController.java

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,12 +1,12 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
-    @RequestMapping("hello")
+    @GetMapping("hello")
     public String testcon(){
         return "hello-test";
     }
